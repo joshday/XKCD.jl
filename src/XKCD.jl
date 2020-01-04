d85e5e2f3607764a1cbe6fa220400d20434249c8
joshday <emailjoshday@gmail.com>
2020-01-04 08:54:49 -0500

    first commit

--- src/XKCD.jl
+++ src/XKCD.jl
@@ -1,5 +1,31 @@
 module XKCD
 
-greet() = print("Hello World!")
+using HTTP
+
+function comic(i::Union{Nothing, Int} = nothing; open=true)
+    myurl = "https://xkcd.com/$(isnothing(i) ? "" : i)"
+    x = split(String(HTTP.get(myurl).body), '\n')
+    i = findfirst(==("""<div id="comic">"""), x)
+    ln = x[i + 1]
+    src = "https:$(cleanmatch(match(r"src=\"(.*?)\"", ln).match))"
+    title = cleanmatch(match(r"title=\"(.*?)\"", ln).match)
+    println(title)
+    if open
+        if Sys.isapple()
+            run(`open $src`)
+        elseif Sys.islinux()
+            run(`xdg-open $src`)
+        elseif Sys.iswindows()
+            run(`start $src`)
+        end
+    end
+    (imgsrc=src, hover=title)
+end
+
+function cleanmatch(x) 
+    x = replace(x, r"(src=|title=|\")" => "")
+    x = replace(x, "&quot;" => '"')
+    x = replace(x, "&#39;" => "'")
+end
 
 end # module
