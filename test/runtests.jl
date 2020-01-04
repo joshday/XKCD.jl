f3da11768c46151283aadcaa4f97dbd2dce46b11
joshday <emailjoshday@gmail.com>
2020-01-04 16:29:15 -0500

    add .travis.yml

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using XKCD
 using Test
 
 @testset "XKCD.jl" begin
-    # Write your own tests here.
+    c = XKCD.comic(open=false)
+    @test c.id >= 2250
+
+    c = XKCD.comic(999, open=false)
+    @test c.id == 999
+    @test c.title == "Cougars"
+    @test c.img == "https://imgs.xkcd.com/comics/cougars.png"
 end
