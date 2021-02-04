0cb2891e6ab4d7652fb89a6e26857d0ef41654e7
joshday <emailjoshday@gmail.com>
2021-02-04 10:36:24 -0500

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,8 @@ using XKCD
 using Test
 
 @testset "XKCD.jl" begin
-    c = XKCD.comic(open=false)
-    @test c.num >= 2250
-
-    c = XKCD.comic(999, open=false)
-    @test c.num == 999
-    @test c.title == "Cougars"
-    @test c.img == "https://imgs.xkcd.com/comics/cougars.png"
+    c = XKCD.Comic(999)
+    @test c.json.num == 999
+    @test c.json.title == "Cougars"
+    @test c.json.img == "https://imgs.xkcd.com/comics/cougars.png"
 end
